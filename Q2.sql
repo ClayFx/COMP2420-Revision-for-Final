4398420915846fd5b67c9d807e4d1eec46c10ea9
Xuan Feng <u6234544@anu.edu.au>
2018-06-02 20:25:26 +1000

    Q4(b) modified

--- Q2.sql
+++ Q2.sql
@@ -17,3 +17,9 @@ from Suppliers, Parts
 where exists ( select Si.s_id, Si.p_id 
 				from Supplier_inventory as Si inner join Requests as R on Si.s_id = R.s_id and Si.p_id = R.p_id 
 				where Si.quantity < R.quantity ) ;
+
+
+select * from 
+(select s_id, p_id, sum(quantity) from requests group by s_id, p_id) as A, (select s_id, name from suppliers) as B, (select s_id, p_id, sum(quantity) 
+from supplier_inventory group by s_id, p_id) as C, (select p_id, name from parts) as D 
+where A.s_id = B.s_id and A.s_id = C.s_id and A.p_id = C.p_id and A.p_id = D.p_id and C.sum < A.sum;
